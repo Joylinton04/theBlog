2f4c06c1342326fec574f2327ec8a6fb5ee35df2
Joylinton04 <adujoy05@gmail.com>
2024-09-22 19:08:35 +0000

    responsive top page

--- src/components/BlogComponent.tsx
+++ src/components/BlogComponent.tsx
@@ -1,19 +1,28 @@
 import img1 from '../assets/img1.jpg'
+interface sizeProps {
+  mdWidth?: boolean;
+  mmdWidth?: boolean;
+  height?: boolean;
+  mdText?: boolean;
+}
 
-
-const BlogComponent = () => {
+const BlogComponent = ({mdWidth, mmdWidth, mdText}:sizeProps) => {
+  const isWidth: string = mdWidth ? "md:w-[200px]" : "md:";
+  const isText: string = mdText ? "md:hidden" : ""
+  const isTextSize: string = mdText ? "md:text-2xl md:font-medium" : "";
+  // const mmdWidth: String = ;
 
   return (
-    <div className="h-[560px]">{/* {/* min-w-[450px] */} 
+    <div className={`h-[600px] md:h-full ${isWidth } lg:max-w-[800px]` }>{/* {/* min-w-[450px] */} 
       <div className="h-full flex flex-col gap-4">
         <div className="h-[45%]">
             <img src={img1} className='h-full w-full object-cover object-center' />
         </div>
         <p className='uppercase font-medium'>Grand theft auto</p>
-        <div className='capitalize w-[95%] text-4xl font-Blogtitle font-bold'>
+        <div className={`capitalize w-[95%] text-4xl font-Blogtitle font-bold ${isTextSize}`}>
             Grand Theft Auto V was realeased 11 years ago
         </div>
-        <div>
+        <div className={`${isText}`}>
             Grand Theft Auto V is one of the most popular video games ever. It has sold over 250 million copies across the world. It was the first in the entertainment sector to make over 1 billion usd in just 3 days...
         </div>
         <h2 className='uppercase font-medium text-sm'>Joylinton</h2>
--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -8,37 +8,45 @@ const Landingpage = () => {
     <div className='min-h-[100vh] font-lora'>
       <Navbar/>
       {/* Layout */}
-
-      <div className='p-6 sm:flex'>
-        {/* Section one */}
-        <div className='flex flex-col gap-6'>
-            <div className='bg-black px-4 py-1 uppercase text-sm text-white'>Today's Picks</div>
-            <div className='sm:hidden'>
-              <div className='flex-col gap-6 flex-wrap'>
-                <BlogComponent/>
-                <BlogComponent/>
-                <BlogComponent/>
+      <div className='flex justify-center items-center'>
+        <div className='p-6 md:flex md:flex-col md:gap-4 lg:flex-row'>
+          {/* Section one */}
+          <div className='flex flex-col gap-6'>
+              <div className='bg-black px-4 py-1 uppercase text-sm text-white'>Today's Picks</div>
+              <div className='md:hidden'>
+                <div className='flex-col gap-6 flex-wrap'>
+                  <BlogComponent/>
+                  <BlogComponent/>
+                  <BlogComponent/>
+                </div>
               </div>
+              <div className='hidden md:flex md:gap-6'>
+                {/* <div>
+                  <BlogComponent/>
+                  <BlogComponent/>
+                </div> */}
+                <div className='md:flex md:flex-col md:gap-6'>
+                  <div><BlogComponent mdWidth mdText/></div>
+                  <div><BlogComponent mdWidth mdText/></div>
+                </div>
+                <div>
+                  <BlogComponent/>
+                </div>
+              </div>
+          </div>
+          
+          {/* Section two */}
+          <div className='flex flex-col gap-6 flex-wrap mt-16 lg:mt-0'>
+            <div className='bg-black px-4 py-1 uppercase text-sm text-white'>Most recent</div>
+            <div className='flex flex-wrap gap-6 lg:flex-col'>
+              <MostRecent/>
+              <MostRecent/>
+              <MostRecent/>
+              <MostRecent/>
             </div>
-            <div>
-              <div><BlogComponent/></div>
-              <div><BlogComponent/></div>
-              <div></div>
-            </div>
-        </div>
-        
-        {/* Section two */}
-        <div className='flex flex-col gap-6 flex-wrap mt-16'>
-          <div className='bg-black px-4 py-1 uppercase text-sm text-white'>Most recent</div>
-          <div className='flex flex-wrap gap-6'>
-            <MostRecent/>
-            <MostRecent/>
-            <MostRecent/>
-            <MostRecent/>
           </div>
         </div>
       </div>
-
     </div>
   )
 }
