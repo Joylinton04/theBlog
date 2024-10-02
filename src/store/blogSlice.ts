b8ba878a60a555b411d3090f7fc6309d13f4cd90
Joylinton04 <adujoy05@gmail.com>
2024-10-02 08:18:44 +0000

    added react redux for state management

---
+++ src/store/blogSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface blog {
+    id: number;
+    title: string;
+    slug: string;
+    content: string;
+    date: Date;
+    author: string;
+    image: string;
+    category: string;
+    // tags: string[];
+}
+interface blogState {
+    blogs: blog[];
+}
+
+
+const initialState: blogState = {
+    blogs: [{
+        id: 1,
+        title: "Cybertruck: The Future of Electric Utility Vehicle",
+        slug: "cybertruck-electric-utility-vehicle",
+        content: "The Tesla Cybertruck has captured the world's attention with its futuristic design, impressive performance, and groundbreaking technology. Unveiled by Elon Musk, the Cybertruck is Tesla's bold step into the electric truck market, promising not just a new type of utility vehicle but also a glimpse into the future of transportation.One of the most striking features of the Cybertruck is its radical design. Unlike traditional trucks with their smooth, curved edges, the Cybertruck has a rugged, angular look, made from ultra-hard stainless steel. This exoskeleton is designed to provide maximum protection to passengers, making it extremely durable and resistant to damage. The unique design is polarizing, but it emphasizes the idea that this truck is unlike anything else on the road.The Cybertruck isn't just about appearances—it also delivers impressive performance. It comes with up to three electric motors, providing it with serious power and an acceleration of 0-60 mph in as little as 2.9 seconds. Its electric range can go up to 500 miles, depending on the model, making it a competitive option even for long-distance use. It also has an impressive towing capacity of over 14,000 pounds and can handle the toughest of terrains with its adaptive air suspension.As expected from a Tesla, the Cybertruck is packed with cutting-edge technology. It features Tesla's autopilot, along with a range of sensors that allow for semi-autonomous driving capabilities. The interior of the truck is futuristic too, with a large central touchscreen and a minimalist layout that focuses on functionality and comfort.One of the biggest selling points of the Cybertruck is its contribution to reducing carbon emissions. The electric motor ensures zero emissions, which is a big win for those who care about the environment. In a world where climate change is a pressing issue, the Cybertruck offers a sustainable alternative to traditional, gas-guzzling trucks without compromising on performance or utility.The Cybertruck isn't just for show—it's a practical utility vehicle. Its spacious bed, called the vault comes with a retractable cover and provides ample space for cargo. It's built for adventure, work, and everything in between. The versatility of the Cybertruck makes it an attractive option for outdoor enthusiasts, workers needing a powerful vehicle, or simply those looking to make a statement.The Tesla Cybertruck is a bold experiment in both design and function. It reimagines what a truck can be, offering an environmentally friendly, high-performing, and durable vehicle that is ready for the future. While its unconventional looks have sparked mixed reactions, there's no denying the technological and practical potential of the Cybertruck. As Tesla continues to innovate, the Cybertruck could very well pave the way for the future of electric utility vehicles.",
+        date: new Date(),
+        author: "Joylinton 2.0",
+        image: "/images/blog/1.jpg",
+        category: "Technology",
+    }],
+}
+
+export const blogSlice = createSlice({
+    name: "blog",
+    initialState,
+    reducers: {
+        addBlog(state, action) {
+            state.blogs.push(action.payload);
+        },
+    },
+})
+export default blogSlice.reducer; 
+export const { addBlog } = blogSlice.actions;
