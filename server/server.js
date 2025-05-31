17f8e39c4ace8afca6167957e4842e2287f16917
Joylinton04 <adujoy05@gmail.com>
2025-05-31 10:25:55 +0000

    server setup up

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.send('API is working')
+})
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log("Server is Up!")
+})
