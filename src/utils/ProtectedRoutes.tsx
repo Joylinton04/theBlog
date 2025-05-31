e256c9d5463b285ca95c4fb0c5f320c2772bba53
Joylinton04 <adujoy05@gmail.com>
2025-05-31 09:57:44 +0000

    separated files

--- src/utils/ProtectedRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom"
-import { useAppSelector } from "../store/store"
-
-const ProtectedRoutes = () => {
- const user = useAppSelector(state => state.auth.isAuthenticated)
- return user ? <Outlet/> : <Navigate to="/login"/>
-}
-
-export default ProtectedRoutes;
