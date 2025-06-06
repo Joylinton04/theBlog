e8c769b1e254c26eb05906f2634d7d149d1cc7e6
Joylinton04 <adujoy05@gmail.com>
2025-06-06 23:34:44 +0000

    custom token endpoint

--- server/route/auth.route.js
+++ server/route/auth.route.js
@@ -7,6 +7,7 @@ const authMiddleware = require('../middleware/auth.middleware')
 
 route.post('/register',authControllers.register)
 route.post('/isAuth',authControllers.isAuthenticated)
+route.post('/custom-token',authControllers.createCustomToken)
 
 
 
