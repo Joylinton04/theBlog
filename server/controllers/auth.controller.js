7c878f1f28169bb0fc00db0049b33fcfdfd06fe6
Joylinton04 <adujoy05@gmail.com>
2025-06-06 23:34:29 +0000

    created custom token route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -21,6 +21,28 @@ const register = async (req, res) => {
   }
 };
 
+const createCustomToken = async (req, res) => {
+  const { email, password } = req.body;
+
+   if (!email || !password) {
+    return res.status(400).json({ success: false, message: "Missing credentials" });
+  }
+
+  try {
+    const {uid} = await admin.auth().getUserByEmail(email);
+
+    const token = await admin.auth().createCustomToken(uid, { isAdmin: false });
+
+    res.status(200).json({
+      success: true,
+      message: "Custom token successfully created",
+      token,
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
 const isAuthenticated = async (req, res) => {
   // verify token 
   const {uid} = req.body
@@ -32,4 +54,4 @@ const isAuthenticated = async (req, res) => {
   }
 }
 
-module.exports = {register, isAuthenticated};
+module.exports = {register, isAuthenticated, createCustomToken};
