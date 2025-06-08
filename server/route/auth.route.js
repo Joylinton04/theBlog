013b51b8b0f5d477b5335e69e3122db515d2399d
Joylinton04 <adujoy05@gmail.com>
2025-06-08 11:49:40 +0000

    route

--- server/route/auth.route.js
+++ server/route/auth.route.js
@@ -8,6 +8,7 @@ const authMiddleware = require('../middleware/auth.middleware')
 route.post('/register',authControllers.register)
 route.post('/isAuth',authControllers.isAuthenticated)
 route.post('/custom-token',authControllers.createCustomToken)
+route.get('/get-users', authMiddleware, authControllers.getUsers)
 
 
 
