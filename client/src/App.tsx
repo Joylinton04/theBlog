31b6282ad848c1d61f92fcf1de88bdc20e6c27c0
Joylinton04 <adujoy05@gmail.com>
2025-06-08 11:50:58 +0000

    axios middleware

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './route/Approute';
-import { useEffect } from 'react';
-import { initializeAuthListener } from './store/FirebaseAuth';
-import { useDispatch } from 'react-redux';
+import { RouterProvider } from "react-router-dom";
+import router from "./route/Approute";
+import { useEffect } from "react";
+import { initializeAuthListener } from "./store/FirebaseAuth";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { getToken } from "./config/tokens";
 
 const App = () => {
   const dispatch = useDispatch();
+
+  axios.interceptors.request.use(
+    async (config) => {
+      const token = await getToken();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
 
   useEffect(() => {
     initializeAuthListener(dispatch);
