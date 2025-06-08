c38fcba4619a9d64853c6ade8a44c4b4b648da75
Joylinton04 <adujoy05@gmail.com>
2025-06-08 11:49:54 +0000

    middleware functionality

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,17 +1,22 @@
 const admin = require("../config/firebase.admin");
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.headers.authorization?.split("Bearer ")[1];
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
-  console.log("Token: ", token)
+   const authHeader = req.headers.authorization;
+   console.log(authHeader)
 
-  try {
-    const decoded = await admin.auth().verifyIdToken(token);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
-  }
+  // if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  //   return res.status(401).json({ success: false, message: "Unauthorized" });
+  // }
+
+  // const idToken = authHeader.split(" ")[1];
+
+  // try {
+  //   const decodedToken = await admin.auth().verifyIdToken(idToken);
+  //   req.user = decodedToken; // attach user info to the request
+  //   next();
+  // } catch (err) {
+  //   return res.status(403).json({ success: false, message: "Invalid token" });
+  // }
 };
 
 module.exports = authMiddleware;
