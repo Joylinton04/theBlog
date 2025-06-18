6c8446c136a59bd62dd2a1b85d44423c93fc41ae
Joylinton04 <adujoy05@gmail.com>
2025-06-18 22:04:26 +0000

    commented out protected route component

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -2,13 +2,15 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import ScrollToTop from '../components/ScrollToTop'
 import ProtectedRoutes from '../utils/ProtectedRoutes';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
       <div className='min-h-[100vh] font-lora text-[#333333] dark:bg-black dark:text-white'>
           <ScrollToTop/>
           <Navbar/>
-          <ProtectedRoutes/>
+          {/* <ProtectedRoutes/> */}
+          <Outlet/>
           <Footer/>
       </div>
   )
